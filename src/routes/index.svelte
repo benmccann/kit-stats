7044eae85d5dd2c651dcc7d3535717abfdc47d9f
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-01-05 08:09:41 -0800

    correct package

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 	import type { Load } from '@sveltejs/kit';
 
 	export const load: Load = async ({ fetch }) => {
-		const res = await fetch('https://api.npmjs.org/downloads/range/2000-01-01:2030-01-01/svelte');
+		const res = await fetch('https://api.npmjs.org/downloads/range/2000-01-01:2030-01-01/@sveltejs/kit');
 		const json = await res.json();
 		let total = 0;
 		const data = json.downloads.map(e => {
@@ -28,7 +28,7 @@
 
 <div id="wrapper" class="container mx-auto mt-12 text-center" style="max-width: 900px">
 
-	<h1>SvelteKit has been downloaded over 10 million times</h1>
+	<h1>SvelteKit has been downloaded nearly 1 million times</h1>
 
 	<div style="position:relative; height:250px">
 		<DownloadsChart data={data} />
